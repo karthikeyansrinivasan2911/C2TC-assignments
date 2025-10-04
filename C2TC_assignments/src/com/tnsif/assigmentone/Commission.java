3f405c4c25549f0658aaa1666b7b96e92f319b14
karth <karth@karthi>
2025-10-04 10:42:28 +0530

    new assignment

--- C2TC_assignments/src/com/tnsif/assigmentone/Commission.java
+++ C2TC_assignments/src/com/tnsif/assigmentone/Commission.java
@@ -1,10 +1,48 @@
 package com.tnsif.assigmentone;
 
-public class Commission {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Commission {
+	    private String name;
+	    private String address;
+	    private String phone;
+	    private double salesAmount;
+	    public void acceptDetails() {
+	        Scanner sc = new Scanner(System.in);
 
+	        System.out.print("Enter Name: ");
+	        name = sc.nextLine();
+
+	        System.out.print("Enter Address: ");
+	        address = sc.nextLine();
+
+	        System.out.print("Enter Phone: ");
+	        phone = sc.nextLine();
+
+	        System.out.print("Enter Sales Amount: ");
+	        salesAmount = sc.nextDouble();
+	    }
+	    public void calculateCommission() {
+	        double commission;
+
+	        if (salesAmount >= 100000) {
+	            commission = salesAmount * 0.10;
+	        } else if (salesAmount >= 50000) {
+	            commission = salesAmount * 0.05;
+	        } else if (salesAmount >= 30000) {
+	            commission = salesAmount * 0.03;
+	        } else {
+	            commission = 0;
+	        }
+
+	        System.out.println("\n--- Employee Commission Details ---");
+	        System.out.println("Name: " + name);
+	        System.out.println("Address: " + address);
+	        System.out.println("Phone: " + phone);
+	        System.out.println("Sales Amount: " + salesAmount);
+	        System.out.println("Commission: " + commission);
+	    }
 	}
+	
 
-}
+
